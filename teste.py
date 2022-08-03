7743c5358b205249360dea968807c660e3762db4
niceias.filho <niceias.rn@hotmail.com>
2022-08-03 17:49:43 -0300

    estudando sobre arquivo

--- teste.py
+++ teste.py
@@ -1,38 +1,40 @@
 
-def soma( a, b):
-    num1 =  a
-    num2 =  b
-    res = a + b
-    print("soma de {} + {} é igua a {}".format(a,b, res))
-
-
-def subtracao():
-    a = int(input("Digite o numerador: "))
-    b = int(input("Digite o numerador: "))
-    return  a - b
-
-
-####PROGRAMA PRINCIPAL
-print("################################################")
-print("BEM VINDO AO TABUADA")
-print("################################################")
-
-a = int(input("Digite o numerador: "))
-b = int(input("Digite o numerador: "))
-
-soma(a, b)
-
-print("#########################################################")
-
-sub = subtracao()
-print("o resultado da subtração é: {} ".format(sub))
+def ler_arquivo():
+    with open("palavrasecreta", "r") as arquivo:
+        linha = arquivo.read()
+        print(linha)
 
 
 
+def escreve_arquivo():
+   pedido = "s"
+   while(pedido == "s"):
+        with open("palavrasecreta", "a", newline="") as arquivo:
+
+            arquivo.write(input("Digite aqui: "))
+            arquivo.write('\n')
+
+        pedido = input("deseja continuar pedindo: ")
 
 
 
+def captura_arquivo():
+    with open("palavrasecreta", "r") as arquivo:
+        lista = []
+        for linha in arquivo:
+            linha =  linha.strip()
+            lista.append(linha)
+        print(lista)
 
+
+leitura = int(input("Digite uma opção: "))
+
+if(leitura == 1):
+    ler_arquivo()
+elif(leitura == 2):
+    escreve_arquivo()
+else:
+    captura_arquivo()
 
 
 
